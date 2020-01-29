b6b62acec3bdd71d634238579b89dd25a10500f1
ved1998karma <58908305+ved1998karma@users.noreply.github.com>
2020-01-29 11:41:58 +0530

    a

---
+++ A.java
@@ -0,0 +1,13 @@
+public classA{
+public static void main(String[]args){
+try
+{
+int data=50/0;
+}
+catch(Arithmetic Exception e)
+{
+System.out.println(e);
+}
+System.out.println("rest of the code");
+}
+}  
